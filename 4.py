98e339475f0e960250d9d1619438e73f8c96b4a1
Ash2611PK <106911651+Ash2611PK@users.noreply.github.com>
2023-01-17 17:39:02 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+Name=input("What's Your Name : ")
+print ("Welcome",Name,"Into This Amazing World" )
+Age= int(input("What's Your Age : "))
+if Age>16:
+    print("We Are Registering Your Details, Wait For A Min. ")
+else :
+    print("Sorry , We Can't Register You As A Player ")
+load=range(1,101)
+for number in load:
+    print (number)
